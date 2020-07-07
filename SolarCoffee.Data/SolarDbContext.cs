bf5391cde503a047813f2f97bcd7e11d0b2529e9
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-07-07 01:02:42 -0700

    Removed Class1.cs

---
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SolarCoffee.Data
+{
+    public class SolarDbContext : IdentityDbContext
+    {
+        
+    }
+}
